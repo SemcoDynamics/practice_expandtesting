2522e581b28cde160ec5ca535d26bfb08a254be5
Brandon Olckers <43698232+SemcoDynamics@users.noreply.github.com>
2024-02-13 20:58:22 +0200

    new commit

--- tests/Add_Remove.spec.js
+++ tests/Add_Remove.spec.js
@@ -7,7 +7,20 @@ test.beforeEach(async ({browser}) => {
 })
 
 test.describe('Add/Remove Elements', () => {
-    test('Input: number', async ({ page }) => {
-        
+    test('Add a new element', async ({ page }) => {
+        await page.goto('/add-remove-elements')
+        await page.getByRole('button', {name: 'Add Element'}).click()
+        await expect(page.getByRole('button', {name: 'Delete'})).toBeVisible()
+        await page.getByRole('button', {name: 'Delete'}).click()
     })
+    test('Add multiple elements', async ({ page }) => {
+        await page.goto('/add-remove-elements')
+        await page.getByRole('button', {name: 'Add Element'}).click()
+        await page.getByRole('button', {name: 'Add Element'}).click()
+        const count = page.getByRole('button', {name: 'Delete'}).count()
+        expect(count).toEqual(2)
+        await expect(page.getByRole('button', {name: 'Delete'})).toBeVisible()
+        await page.getByRole('button', {name: 'Delete'}).click()
+    })
+    
 })
